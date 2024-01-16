18e503e273d827a3c7683f3d405ac8b8560ce51d
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2024-01-16 15:55:26 -0600

    Tercer Commit, creo

--- JavaScript.js
+++ JavaScript.js
@@ -1,6 +1,18 @@
 
-var palabras = [["Leonel Pepsi"], ["Cristiano Frionaldo"], [""]];
+var palabras = ["Leonel Pepsi", "Cristiano Frionaldo", "Pele", "Pepe", "Neymar", "De Bruyne", "Ronaldo Panzario", "Luis Suarez", "Jordi Alba"];
 
 function Iniciar(){
-    console.log("Que hubo perro");
+    var encontrar = (Math.random() * 3).toFixed(0);
+    console.log(encontrar);
+
+    console.log(palabras[encontrar]);
+
+    var encontrada = (palabras[encontrar]);
+
+    var spaceCount = (encontrada.split("").length-1);
+    var spaceCount2 = spaceCount + 1;
+    console.log(spaceCount2);
+
+    
+
 }
