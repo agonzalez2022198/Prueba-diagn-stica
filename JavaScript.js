9f9078b25df0dd8804459f0edbce1aebc2f9175a
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2024-01-16 16:34:47 -0600

    Ultimo Commit

--- JavaScript.js
+++ JavaScript.js
@@ -1,5 +1,6 @@
 
 var palabras = ["Leonel Pepsi", "Cristiano Frionaldo", "Pele", "Pepe", "Neymar", "De Bruyne", "Ronaldo Panzario", "Luis Suarez", "Jordi Alba"];
+var oculta = [];
 
 function Iniciar(){
     var encontrar = (Math.random() * 3).toFixed(0);
@@ -13,6 +14,16 @@ function Iniciar(){
     var spaceCount2 = spaceCount + 1;
     console.log(spaceCount2);
 
+    for (var i = 0; i < spaceCount2; i++) {
+        oculta[i] = "_ ";
+
+        
+    }
+     var final = oculta.join('');
+
+     document.getElementById('guiones').textContent = final;
+
+     document.getElementById('adivinar').innerHTML =+ "<input type="+"text"+">";
     
 
 }
